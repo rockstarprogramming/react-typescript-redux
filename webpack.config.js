09c2c374f2ac47eefc160ff9c5dd2a602b9d1ea4
RSP <rsp@github.com>
2017-11-07 15:18:32 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+module.exports = {
+  entry: './client/index.tsx',
+  output: {
+    path: path.resolve('dist'),
+    publicPath: "/dist/",
+    filename: 'bundle.js'
+  },
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+  },
+  module: {
+    loaders: [
+      { test: /\.tsx$/, loader: 'awesome-typescript-loader' },
+    ],
+  }
+}
